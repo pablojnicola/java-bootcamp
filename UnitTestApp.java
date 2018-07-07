463322d1f33d32342415ccc88501d193a4a0c798
pablojnicola <40781955+pablojnicola@users.noreply.github.com>
2018-07-07 01:46:08 -0300

    Update UnitTestApp.java

--- UnitTestApp.java
+++ UnitTestApp.java
@@ -1,6 +1,5 @@
 package com.globant.boopcamp;
 
-import main.App;
 import org.apache.log4j.Logger;
 
 /**
@@ -9,7 +8,7 @@ import org.apache.log4j.Logger;
  */
 public class UnitTestApp {
     
-    static Logger log = Logger.getLogger(App.class);
+    static Logger log = Logger.getLogger(UnitTestApp.class);
 
   public static void main( String[] args ){
     log.debug( "Maven dependency log4j..." );
