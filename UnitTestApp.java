74b76bc8baa308fab47199ecaca8b1d2a12cc8cb
pablojnicola <40781955+pablojnicola@users.noreply.github.com>
2018-07-07 01:40:48 -0300

    Maven Exercise
    Create and build a simple Maven project:
    Customize the Maven project by adding new dependencies: log4j, junit.
    Create a simple unit test under src/test/java and run it. Then skip the unit test by property or by adding the skipping test configuration to your pom.xml file.

---
+++ UnitTestApp.java
@@ -0,0 +1,18 @@
+package com.globant.boopcamp;
+
+import main.App;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Pablo
+ */
+public class UnitTestApp {
+    
+    static Logger log = Logger.getLogger(App.class);
+
+  public static void main( String[] args ){
+    log.debug( "Maven dependency log4j..." );
+  }
+    
+}
