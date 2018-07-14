2cfc756faddbb61c1230fe6dfffe725ac0eb9888
pablojnicola <40781955+pablojnicola@users.noreply.github.com>
2018-07-14 01:54:59 -0300

    Topic 1 - Maven Project - (corrected)

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.globant.bootcamp.MVUnitTestCalculator;
+
+public class Calculator {
+	
+	public static int sum(int a, int b) {
+		
+		return a+b;
+		
+	}
+	
+	public static int rest(int a, int b) {
+		
+		return a-b;
+		
+	}
+	
+}
